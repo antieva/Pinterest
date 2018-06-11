7173f98711badc400630332c74f47ca8a05427f0
Eva Antipina <eva.antipina@gmail.com>
2018-06-10 20:33:52 -0700

    add isUniqueUser() and addUser() functions.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,22 @@ import { User } from './models/user.model';
 export class AppComponent {
   title = 'app';
 
-  
+  users: User[] = [
+  new User('emailOne@gmail.com', '123password', 'femail', 'English', 'USA', ['Home decor', 'Makeup', 'Food and drink', 'Humor', 'Art']),
+  new User('emailTwo@gmail.com', '456password', 'femail', 'English', 'USA', ['Home decor', 'Makeup', 'Food and drink', 'Humor', 'Art']),
+  new User('emailThree@gmail.com', '111password', 'male', 'English', 'USA', ['Home decor', 'Healthy Snacks', 'Food and drink', 'Humor', 'Workouts'])
+  ];
+
+  isUniqueUser(newUser: User) {
+    for (let user of this.users) {
+      if (user[0] === newUser[0] && user[1] === newUser[1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  addUser(newUser: User) {
+    this.users.push(newUser);
+  }
 }
