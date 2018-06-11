df43199e86e4f34730bbc3be34b70b819c311cc6
Eva Antipina <eva.antipina@gmail.com>
2018-06-10 20:39:45 -0700

    Change User class in user.model.ts.

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,4 +1,8 @@
 export class User {
   public name: string = "";
-  constructor(public email: string, public password: string, public gender: string, public laguage: string, public country: string, public interests: string[]) { }
+  public gender: string = "";
+  public laguage: string = "";
+  public country: string = "";
+  public interests: string[] = [];
+  constructor(public email: string, public password: string) { }
 }
