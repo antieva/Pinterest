14146b42ac3c24656310502a3f67d300391353b2
Eva Antipina <eva.antipina@gmail.com>
2018-06-15 14:56:19 -0700

    Add methods to retrieve data from Service.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../account.service';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [AccountService]
 })
 export class LoginComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(private router: Router, private albumService: AccountService) {}
 
   ngOnInit() {
   }
+
+  goToAccount(loggedinUser: User) {
+    this.router.navigate(['accounts', loggedinUser.id]);
+  };
 
 }
