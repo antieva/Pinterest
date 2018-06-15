98dc6a58b3dd6a8521a01cc55f3b5d4055cb514e
Eva Antipina <eva.antipina@gmail.com>
2018-06-15 12:43:31 -0700

    Clean the code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,12 @@ import { User } from './models/user.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
 
   users: User[] = [
   new User('emailOne@gmail.com', '123password'),
   new User('emailTwo@gmail.com', '456password'),
   new User('emailThree@gmail.com', '111password')
   ];
-
-  isUniqueUser(email:string) {
-    for (let user of this.users) {
-      if (user.email === email) {
-        return false;
-      }
-    }
-    return true;
-  }
 
   addUser(newUser: User) {
     for (let user of this.users) {
