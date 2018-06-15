6b831980f4dab30562dbb6f4a1c579fbf14e7286
Eva Antipina <eva.antipina@gmail.com>
2018-06-15 11:51:39 -0700

    Fix the bug with error message.

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -13,7 +13,18 @@ export class NewUserComponent {
     let newUser: User = new User(email, password);
     this.sendUser.emit(newUser);
   }
-  errorMessage() {
-    this.sendUser.emit(false);
+  users: User[] = [
+  new User('emailOne@gmail.com', '123password'),
+  new User('emailTwo@gmail.com', '456password'),
+  new User('emailThree@gmail.com', '111password')
+  ];
+
+  isUniqueUser(email:string) {
+    for (let user of this.users) {
+      if (user.email === email) {
+        return false;
+      }
+    }
+    return true;
   }
 }
