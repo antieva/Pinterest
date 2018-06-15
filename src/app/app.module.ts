8f44624964a1e030d498ffb350f0603bb75db5ce
Eva Antipina <eva.antipina@gmail.com>
2018-06-15 13:58:00 -0700

    Hiding Firebase Credentials.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NewUserComponent } from './new-user/new-user.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
